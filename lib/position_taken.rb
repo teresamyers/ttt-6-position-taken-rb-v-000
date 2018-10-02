51673557f87faf25b6d55f8f8c866d45e3f0da0c
teresa myers <teresamarie04@gmail.com>
2018-10-02 04:19:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, position)
+  taken = nil
+  if board[position] == " "
+    taken = false
+  elsif board[position] == ""
+    taken = false
+  elsif board[position] == nil
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
