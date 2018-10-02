f2ef1469c12704c3fc19fdefe317a5bc2663a003
teresa myers <teresamarie04@gmail.com>
2018-10-02 04:01:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,3 +5,17 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def move (array, index, character = "X")
   array[index_number] = character
 end
+
+def position_taken?(board, position)
+  taken = nil
+  if board[position] == " "
+    taken = false
+  elsif board[position] == ""
+    taken = false
+  elsif board[position] == nil
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
