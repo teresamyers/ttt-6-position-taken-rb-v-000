eb82c4b0688d2804ce951c9837fe70645ac60bc6
teresa myers <teresamarie04@gmail.com>
2018-10-02 03:53:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,17 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+puts "Welcome to Tic Tac Toe!"
+ display_board(board)
+
+ puts "Where would you like to go?"
+ input = gets.strip
+
+ index = input_to_index(input)
+ move(board, index, "X")
+
+ display_board(board)
+
 def input_to_index(input)
   index = input.to_i - 1
 end
