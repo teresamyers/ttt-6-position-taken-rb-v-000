75fc096738ae8653b8bd75977f6d49ba4f8d0f30
teresa myers <teresamarie04@gmail.com>
2018-10-02 03:52:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+index = input_to_index(input)
+move(board, index, "X")
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move (array, index, character = "X")
+  array[index] = character
+end
