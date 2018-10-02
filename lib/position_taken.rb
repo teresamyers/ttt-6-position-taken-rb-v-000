1aa6e8d3bf63c86bfbac9b2cb57d18378ef5ca8a
teresa myers <teresamarie04@gmail.com>
2018-10-02 03:54:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,6 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def input_to_index(input)
-  index = input.to_i - 1
-end
-
 def move (array, index, character = "X")
   array[index] = character
 end
